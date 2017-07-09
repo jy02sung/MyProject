a6906aadba6937a4933e394e30fd6b2560aff30b
jinwookwak <kwakjinu@gmail.com>
2017-07-09 17:09:00 +0900

    UI 만드는 중
    .

---
+++ Assets/GameStartButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStartButton : MonoBehaviour
+{
+
+    public void Button()
+    {
+        Invoke("startgame", 0.3f);
+    }
+    private void startgame()
+    {
+        SceneManager.LoadScene("ChooseLevel");
+    }
+}
---
+++ Assets/LevelOneButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTwoButton : MonoBehaviour
+{
+
+    public void Button()
+    {
+        Invoke("LevelTwo", 0.3f);
+    }
+    private void LevelTwo()
+    {
+        SceneManager.LoadScene("LevelTwo");
+    }
+}
