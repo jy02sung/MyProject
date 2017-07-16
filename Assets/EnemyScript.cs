571b620c09201b45cbfca03546598f4cc1b76440
jy02sung <jy02sung@naver.com>
2017-07-16 19:28:33 +0900

    적 나오는 스크립트 작성
    웨이브, 적 나오는 주기 등

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float speed;
+    int step = 0;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (step == 0)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, speed);
+            if (transform.position.y >= 1.6f)
+            {
+                transform.Rotate(0.0f, 0.0f, -90.0f);
+                step = 1;
+            }
+        }
+        else if (step == 1)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(speed, 0.0f);
+            if (transform.position.x >= 4.8f)
+            {
+                transform.Rotate(0.0f, 0.0f, -90.0f);
+                step = 2;
+            }
+        }
+        else if (step == 2)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, -speed);
+            if (transform.position.y <= -5.5f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/EnemySpawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour {
+    public GameObject Enemy;
+    public float SpawnTime;
+    public float WaitTime;
+    float time;
+    public int wave;
+    int EnemyNumber;
+
+	void Update ()
+    {
+        time += Time.deltaTime;
+        if (EnemyNumber <= wave)
+        {
+            if (time >= SpawnTime)
+            {
+                EnemyNumber += 1;
+                Instantiate(Enemy);
+                time = 0.0f;
+            }
+        }
+        else
+        {
+            if (time >= WaitTime)
+            {
+                EnemyNumber = 0;
+            }
+        }
+	}
+}
