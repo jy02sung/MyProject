18aa93a15574da604e171403be05e57aeac68058
jy02sung <jy02sung@naver.com>
2017-09-10 19:23:22 +0900

    돈
    재화 추가함

---
+++ Assets/Scripts/PlayerMoney.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerMoney : MonoBehaviour
+{
+    public int Money;
+    public Text MoneyText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Money=10;
+	}
+    public void AddMoney(int MoneyToAdd)
+    {
+        Money += MoneyToAdd;
+    }
+    public bool SubtractMoney(int Price)
+    {
+         if(Money - Price < 0)
+         {
+            Debug.Log("Insufficient Funds");
+            return false;
+         }
+         else
+         {
+             Money -= Price;
+            return true;
+         }
+    }
+
+    void Update()
+    {
+        MoneyText.text = Money.ToString();
+    }
+}
