0e5abde3ccc14249755ba2545e84e2041ae25930
soonjeong <woak_uu@naver.com>
2017-10-29 18:56:51 +0900

    웨이브 제대로 분리

--- Assets/Enemy Scripts/EnemySpawn2.cs
+++ Assets/Enemy Scripts/EnemySpawn2.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemySpawn2 : MonoBehaviour {
+    public GameObject Enemy;
+    public float SpawnTime;
+    public float WaitTime;
+    float time;
+    public int wave;
+    int EnemyNumber;
+    public bool IsStart = false;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        if (!IsStart)
+        {
+            return;
+        }
+        time += Time.deltaTime;
+        if (EnemyNumber <= wave)
+        {
+            if (time >= SpawnTime)
+            {
+                EnemyNumber += 1;
+                Instantiate(Enemy);
+                time = 0.0f;
+            }
+        }
+        else
+        {
+            if (time >= WaitTime)
+            {
+                EnemyNumber = 0;
+                IsStart = false;
+            }
+        }
+    }
 }
--- Assets/Scripts/StartWave.cs
+++ Assets/Scripts/StartWave.cs
@@ -1,26 +1,94 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+[System.Serializable]
+public class waveInfo
+{
+    public GameObject Enemy;
+    public int count;
+    public float delay;
+}
 
-public class StartWave : MonoBehaviour {
-    public List <GameObject> spawner;
-	// Use this for initialization
-	void Start () {
-		
-	}
+[System.Serializable]
+public class wave
+{
+    public List<waveInfo> waveList;
+}
+
+public class StartWave : MonoBehaviour
+{
+    public List<wave> waves;
+    public int waveIndex;
+    public GameObject text;
+    public GameObject button;
+    bool start = false;
+    List<float> coolTime = new List<float>();
+    List<int> spawnNum = new List<int>();
+
 
     public void Button()
     {
         Invoke("startgame", 0.3f);
     }
+
     private void startgame()
     {
-        foreach (GameObject S in spawner)
+        start = true;
+        coolTime.Clear();
+        spawnNum.Clear();
+
+        for (int i = 0; i < waves[waveIndex].waveList.Count; i++)
         {
-            S.GetComponent<EnemySpawn>().IsStart = true;
+            coolTime.Add(0.0f);
+            spawnNum.Add(0);
         }
-        Destroy(gameObject);
+    }
+
+    void Update()
+    {
+        if (!start)
+            return;
+
+        wave nowWave = waves[waveIndex];
+
+        for(int i = 0; i < nowWave.waveList.Count; i++)
+        {
+            if (spawnNum[i] < nowWave.waveList[i].count)
+            {
+                coolTime[i] += Time.deltaTime;
+
+                if (coolTime[i] >= nowWave.waveList[i].delay)
+                {
+                    Instantiate(nowWave.waveList[i].Enemy);
+                    spawnNum[i]++;
+                    coolTime[i] -= nowWave.waveList[i].delay;
+                }
+            }
+        }
+
+        if(isAllSpawn())
+        {
+            if (GameObject.FindGameObjectsWithTag("Enemy").Length == 0)
+            {
+                button.SetActive(true);
+                start = false;
+                waveIndex++;
+                text.GetComponent<Text>().text = "Wave " + (waveIndex + 1).ToString();
+            }
+        }
+    }
+
+    bool isAllSpawn()
+    {
+        wave nowWave = waves[waveIndex];
+        for (int i = 0; i < nowWave.waveList.Count; i++)
+        {
+            if (spawnNum[i] < nowWave.waveList[i].count)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
-
-
